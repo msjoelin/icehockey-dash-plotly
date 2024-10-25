804e6fd3b17b1dec17cd5f5985ca3e347938cbaa
Marcus Sjölin <marcussjolin89@gmail.com>
2024-10-26 00:09:23 +0200

    updates

--- app.py
+++ app.py
@@ -19,32 +19,42 @@ client = bigquery.Client.from_service_account_json(key_path)
 # Get the data from bigquery
 
 q_tblpos = """
-select 
-table_position, team, matchday, points_cum as P, league, season, max_matchday, 
-concat(scored_cum, "-",conceded_cum) as GD, last_5_games
-from (
-  SELECT team, matchday, max(matchday) over (partition by team, schedule_id) as max_matchday, scored_cum, conceded_cum, points_cum, table_position , result, league, season, 
-  STRING_AGG(result) over (partition by team, schedule_id order by date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) as last_5_games
+  SELECT team, matchday, 
+  max(matchday) over (partition by team, schedule_id) as max_matchday, 
+  scored_cum, conceded_cum, 
+  points_cum as P, table_position , result, league, season, 
+  STRING_AGG(result) over (partition by team, schedule_id order by date ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) as last_5_games, 
+  concat(scored_cum, "-",conceded_cum) as GD
   FROM `sportresults-294318.games_data.swehockey_team_games` 
   where 1=1 
   and score is not null 
-)
-order by points_cum desc, table_position asc   
+ order by points_cum desc, table_position asc   
 """
 
 q_tblpos_per_season = """
-SELECT team, matchday, points_cum, table_position, season, count(*) over (partition by matchday, season) as matchday_cnt 
+SELECT team, matchday, points_cum, table_position, season, league, count(*) over (partition by matchday, season) as matchday_cnt 
 FROM `sportresults-294318.games_data.swehockey_team_games` 
-where league = 'shl' and matchday = 11 and score is not null 
+where 1=1 
+and score is not null 
 order by matchday, table_position 
 """
 
 
-# Execute the query and convert the results to a pandas DataFrame
-df_tblpos = client.query(q_tblpos).to_dataframe()
+df_team_games = pd.read_csv("C:/Users/marcu/Documents/github/icehockey-dash-plotly/data/swehockey_team_games_dashboard.csv", low_memory=False)
+
+# Convert all object columns to strings
+for col in df_team_games.select_dtypes(include=['object']).columns:
+    df_team_games[col] = df_team_games[col].astype(str)
 
 
-df_tblposperseason = client.query(q_tblpos_per_season).to_dataframe()
+# df_tblpos = client.query(q_tblpos).to_dataframe()
+df_tblpos = df_team_games[df_team_games['game_id'].notnull()].sort_values(
+    ['points_cum', 'table_position'], 
+    ascending = [False, True])
+
+
+# df_tblposperseason = client.query(q_tblpos_per_season).to_dataframe()
+df_tblposperseason =  df_team_games[df_team_games['game_id'].notnull()]
 
 
 # Function to map results to icons 
@@ -79,7 +89,6 @@ app = dash.Dash(__name__,
                 title='Icehockey Data Dashboard')
 
 
-
 app.layout = html.Div([
     dbc.Container([
         # Headline
@@ -95,8 +104,8 @@ app.layout = html.Div([
                     active_tab='tab-1',
                     children=[
                         dbc.Tab(label='Table', tab_id='tab-1'),
-                        dbc.Tab(label='Points and table position', tab_id='tab-2'),
-                        dbc.Tab(label='Number of goals', tab_id='tab-3'),
+                        dbc.Tab(label='Table Position by matchday', tab_id='tab-2'),
+                        dbc.Tab(label='Point Distribution', tab_id='tab-3'),
                         dbc.Tab(label='Team Statistics', tab_id='tab-4'),
                         dbc.Tab(label='Season Overview', tab_id='tab-5'),
                     ],
@@ -104,30 +113,63 @@ app.layout = html.Div([
                 )
             ])
         ]),
-        dbc.Row(
-            [
+        dbc.Row(id='dropdown-container', className = 'dropdown-flex', children =[
             dbc.Col(
-            dcc.Dropdown(
-            id='league-dropdown',
-            options=[{'label': cat, 'value': cat} for cat in df_tblpos['league'].unique()],
-            value = 'shl',
-            placeholder='Select league',
-           className='dash-dropdown'  
-        ),width=6
-            ),
-              dbc.Col(
-        dcc.Dropdown(
-            id='season-dropdown',
-            options=[{'label': grp, 'value': grp} for grp in df_tblpos['season'].unique()],
-            value = '2024/25',
-            placeholder='Select season',
+                dcc.Dropdown(
+                id='league-dropdown',
+                options=[{'label': cat, 'value': cat} for cat in df_tblpos['league'].unique()],
+                value = 'shl',
+                placeholder='Select league',
             className='dash-dropdown'  
-            ),width=6
+                ),width=2
             ),
-    ], id='dropdown-container', style={'marginBottom': '10px', 'marginTop': '10px'}
+            dbc.Col(
+                dcc.Dropdown(
+                id='season-dropdown',
+                options=[{'label': grp, 'value': grp} for grp in df_tblpos['season'].unique()],
+                value = '2024/25',
+                placeholder='Select season',
+                className='dash-dropdown'  
+            ),width=2
+            ),
+            dbc.Col(
+                dcc.Dropdown(
+                id='homeaway-dropdown',        
+                options=[
+                         {'label': 'Home', 'value': 'home'},
+                         {'label': 'Away', 'value': 'away'},
+                         {'label': 'Total', 'value': 'total'}
+                ],
+                value = 'total',
+                placeholder='Select home/away',
+                className='dash-dropdown'  
+            ),width=2
+            ),
+            dbc.Col(
+                dcc.Dropdown(
+                id='matchday-dropdown',        
+                options=[{'label': grp, 'value': grp} for grp in df_tblpos['matchday'].unique()],
+                value = 52,
+                placeholder='Select matchday',
+                className='dash-dropdown'  
+            ),width=2
+            ),
+             dbc.Col(
+                dcc.Dropdown(
+                id='team-dropdown',        
+                options=[{'label': grp, 'value': grp} for grp in df_tblpos['team'].unique()],
+                value = 'Leksands IF',
+                placeholder='Select team',
+                className='dash-dropdown' , 
+                searchable=True
+            ),width=2
+            )
+            ],  style={'marginBottom': '10px', 'marginTop': '10px'}
           )
 
-        ,dcc.Store(id='df-season-league', data={}),  # Hidden storage for filtered DataFrame        
+        ,dcc.Store(id='df-season-league', data={}),  # Hidden storage for filtered DataFrame      
+        dcc.Store(id='df-pos-matchday', data={}),  # Hidden storage for filtered DataFrame        
+        dcc.Store(id='df-team-filtered', data={}),  # Hidden storage for filtered DataFrame        
         # Content based on tab selection, wrapped in a card
         dbc.Row([
             dbc.Col(
@@ -142,49 +184,74 @@ app.layout = html.Div([
 
 
 @app.callback(
-    [Output('league-dropdown', 'style'),
-     Output('season-dropdown', 'style')],
+    [Output('league-dropdown', 'className'),
+     Output('season-dropdown', 'className'), 
+     Output('homeaway-dropdown', 'className'),
+     Output('matchday-dropdown', 'className'),
+     Output('team-dropdown', 'className')],  
     [Input('tabs', 'active_tab')]
 )
 def update_dropdown_visibility(active_tab):
     if active_tab == 'tab-1':
         # Show dropdown 1, hide dropdown 2
-        return {'display': 'block'}, {'display': 'block'}
+        return 'dash-dropdown', 'dash-dropdown', 'dash-dropdown','hidden-dropdown','hidden-dropdown'
     elif active_tab == 'tab-2':
         # Show dropdown 2, hide dropdown 1
-        return {'display': 'block'}, {'display': 'block'}
-    # Default case, hide both
-    return {'display': 'none'}, {'display': 'none'}
+        return 'dash-dropdown', 'dash-dropdown', 'dash-dropdown','hidden-dropdown','hidden-dropdown'
+    elif active_tab == 'tab-3':
+        # Show dropdown 2, hide dropdown 1
+        return 'dash-dropdown', 'hidden-dropdown', 'hidden-dropdown','dash-dropdown','hidden-dropdown'
+    elif active_tab == 'tab-4':
+        # Show dropdown 2, hide dropdown 1
+        return 'hidden-dropdown', 'hidden-dropdown', 'hidden-dropdown','hidden-dropdown', 'dash-dropdown'
+    # Default hide all 
+    return 'hidden-dropdown', 'hidden-dropdown', 'hidden-dropdown', 'hidden-dropdown', 'hidden-dropdown'
 
 
 
 
 # Callback to update the DataTable based on selected tab and dropdown
 @app.callback(
-    Output('df-season-league', 'data'),
-    [Input('tabs', 'active_tab'),  # Changed to active_tab
-     Input('league-dropdown', 'value'),
-     Input('season-dropdown', 'value')]
+    [Output('df-season-league', 'data'),
+     Output('df-pos-matchday', 'data'), 
+     Output('df-team-filtered', 'data')],
+    [Input('league-dropdown', 'value'),
+     Input('season-dropdown', 'value'),
+     Input('matchday-dropdown', 'value'),
+     Input('team-dropdown', 'value')]
 )
 
-def update_table(selected_tab, selected_league, selected_season):
+def update_table(selected_league, selected_season, selected_matchday, selected_team):
     df_season_league = pd.DataFrame()
-    df_season_league = df_tblpos[(df_tblpos['league'] == selected_league) & (df_tblpos['season'] == selected_season)]
+    df_season_league = df_tblpos[(df_tblpos['league'] == selected_league) & 
+                                 (df_tblpos['season'] == selected_season)]
 
-    return df_season_league.to_dict('records')
+    df_pos_matchday_league = pd.DataFrame()
+    df_pos_matchday_league = df_tblposperseason[(df_tblposperseason['league'] == selected_league) & 
+                                                (df_tblposperseason['matchday'] == selected_matchday)]
+    
+    df_team_filtered = df_tblpos[(df_tblpos['team'] == selected_team) & (df_tblpos['league'] != 'preseason')]
+
+    return df_season_league.to_dict('records'), df_pos_matchday_league.to_dict('records'), df_team_filtered.to_dict('records')
 
 
 # Use the filtered data for content rendering
 @app.callback(
     Output('tab-content', 'children'),
     [Input('tabs', 'active_tab'),
-     Input('df-season-league', 'data')]
+     Input('df-season-league', 'data'),
+     Input('df-pos-matchday', 'data'), 
+     Input('df-team-filtered', 'data')]
 )
 
-def render_content(selected_tab, df_season_league):
+def render_content(selected_tab, df_season_league, df_pos_matchday_league, df_team_filtered):
     df_tblpos_filtered = pd.DataFrame(df_season_league)
 
     df_season_league_max = df_tblpos_filtered[df_tblpos_filtered['max_matchday'] == df_tblpos_filtered['matchday']]
+
+    df_pos_matchday_filtered =  pd.DataFrame(df_pos_matchday_league)
+
+    df_teamstat = pd.DataFrame(df_team_filtered)
 
     # Content rendering logic for each tab
     if selected_tab == 'tab-1':
@@ -192,9 +259,9 @@ def render_content(selected_tab, df_season_league):
     elif selected_tab == 'tab-2':
         return tab_content_points(df_tblpos_filtered)
     elif selected_tab == 'tab-3':
-        return tab_content_goals()
+        return tab_content_pointdistr(df_pos_matchday_filtered)
     elif selected_tab == 'tab-4':
-        return tab_content_statistics()
+        return tab_content_teamstat(df_teamstat)
     elif selected_tab == 'tab-5':
         return tab_content_overview()
 
@@ -209,8 +276,8 @@ def tab_content_table(df_season_league_max):
                     columns=[
                         {"name": "Team", "id": "team"},
                         {"name": "GP", "id": "matchday"}, 
-                        {"name": "Points", "id": "P"}, 
-                        {"name": "GD", "id": "GD"}, 
+                        {"name": "Points", "id": "points_cum"}, 
+                        {"name": "GD", "id": "goal_difference"}, 
                         {"name": "Last 5", "id": "last_5_icons", "presentation": "markdown"}, 
                     ],
                     data=df_season_league_max.to_dict('records'),
@@ -246,10 +313,6 @@ def tab_content_points(df_tblpos_filtered):
     fig_tblpos = px.line(df_tblpos_filtered.sort_values(by=['team', 'matchday']), x='matchday', y='table_position', color='team', title='Table Position by Matchday')
     fig_tblpos.update_yaxes(range=[15, 0])
 
-    fig_tblpos_distr = px.box(df_tblposperseason, x='season', y='points_cum', color='season', title='Point Distribution per Season', points='all', boxmode='overlay',
-                                category_orders={'season': sorted(df_tblposperseason['season'].unique())}
-                                )
-
     return dbc.Container([
         dbc.Row([
             dbc.Col(
@@ -259,6 +322,28 @@ def tab_content_points(df_tblpos_filtered):
                 )
             )
         ]),
+     ])
+
+def tab_content_pointdistr(df_pos_matchday):
+
+    fig_tblpos_distr = px.box(df_pos_matchday, 
+                              x='season', 
+                              y='points_cum', 
+                              color='season', 
+                              title='Point Distribution per Season', 
+                              points='all', 
+                              boxmode='overlay',
+                            category_orders={'season': sorted(df_pos_matchday['season'].unique())},
+                            custom_data=['team'] 
+                                )
+    
+    fig_tblpos_distr.update_traces(
+    hovertemplate="<b>Team:</b> %{customdata[0]}<br>" +
+                  "<b>Points:</b> %{y}<br>" +
+                  "<b>Season:</b> %{x}<extra></extra>"
+                  )
+
+    return dbc.Container([
         dbc.Row([
             dbc.Col(
                 dcc.Graph(
@@ -269,11 +354,29 @@ def tab_content_points(df_tblpos_filtered):
         ])
      ])
 
-def tab_content_goals():
-    return html.Div("Content for Number of goals")
+def tab_content_teamstat(df_teamstat):
 
-def tab_content_statistics():
-    return html.Div("Content for Team Statistics")
+    fig_scatter_team = px.scatter(
+        df_teamstat[df_teamstat['matchday'] == df_teamstat['max_matchday']].sort_values(by='season'),
+        x='season',        
+        y='table_position',     
+        color='league',    
+        title='Scatter Plot of Points vs Assists',
+        # labels={'points': 'Points Scored', 'assists': 'Assists'}
+    )
+
+    fig_scatter_team.update_yaxes(autorange='reversed')
+
+    return dbc.Container([
+        dbc.Row([
+            dbc.Col(
+                dcc.Graph(
+                    id='fig_scatter_team',
+                    figure=fig_scatter_team
+                )
+            )
+        ])
+     ])
 
 def tab_content_overview():
     return html.Div("Content for Season Overview")
